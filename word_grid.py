7089c1d45758b5d62c3dd011193a1eea092d4bda
mkelley24 <matthew_kelley@uri.edu>
2023-06-01 21:39:59 -0400

    working on left diagonal list getter

--- word_grid.py
+++ word_grid.py
@@ -71,7 +71,20 @@ class WordGrid:
         return line
 
     def get_left_diagonal_line_list(self):
-
+        left_diagonal_line_list: List[str] = []
+        i: int = 1
+        j: int = 0
+        temp: int = 0
+        left_diagonal_line_list.append(self._get_left_diagonal_line(0, 0))
+        while i < len(self.grid) and j < len(self.grid[0]):
+            left_diagonal_line_list.append(self._get_left_diagonal_line(i, j))
+            if (i + j) % 2 == 0:
+                j += 1
+            else:
+                temp = i
+                i = j
+                j = temp
+        return left_diagonal_line_list
 # helper function
 def string_to_list(line: str) -> List[str]:
     letter_list: List[str] = []

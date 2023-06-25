fa568b271794922346f3dd828348a65a1049bf61
mkelley24 <matthew_kelley@uri.edu>
2023-06-24 23:46:05 -0400

    added to gui, with a button to select a file

--- display.py
+++ display.py
@@ -1,4 +1,19 @@
-import tkinter
+import tkinter as tk
+from tkinter.filedialog import askopenfile
 
-window = tkinter.Tk()
+window = tk.Tk()
+
+canvas = tk.Canvas(window, width=600, height=300)
+canvas.grid(columnspan=3, rowspan=3)
+
+def open_file():
+    file = askopenfile(parent=window, mode='rb', title="Choose a file", filetype=[("Text file", "*.txt")])
+    if file:
+        pass
+
+sudoku_btn_text = tk.StringVar()
+sudoku_btn = tk.Button(window, textvariable=sudoku_btn_text, command=lambda:open_file())
+sudoku_btn_text.set("Select Sudoku Puzzle")
+sudoku_btn.grid(column=1, row=2)
+
 window.mainloop()

7628c028016924f8cc969bb8dc43f9254d305afc
mkelley24 <matthew_kelley@uri.edu>
2023-07-23 17:33:39 -0400

    point methods added

--- test_window.py
+++ test_window.py
@@ -2,6 +2,14 @@ from typing import List
 from window import *
 from point import Point
 
+def compare_lists(letter_list, string_list):
+        if len(letter_list) != len(string_list):
+            return False
+        else:
+            for i in range(len(letter_list)):
+                if letter_list[i].letter_value != string_list[i]:
+                    return False
+            return True
 
 def test_get_window_text():
     test_lines: List[List[str]] = [
@@ -12,7 +20,7 @@ def test_get_window_text():
     test_grid: WordGrid = WordGrid(test_lines)
     start:Point = Point(0, 0)
     test_window: HorizontalWindow = HorizontalWindow(test_grid, 3, start)
-    assert(test_window.text == ["a", "b", "c"])
+    assert(compare_lists(test_window.text, ["a", "b", "c"]))
 
 def test_compare_word_same():
     test_lines: List[List[str]] = [
@@ -25,3 +33,5 @@ def test_compare_word_same():
     test_window: HorizontalWindow = HorizontalWindow(test_grid, 3, start)
     print(test_window.text)
     assert(test_window.compare_word_to_window("abc"))
+
+    

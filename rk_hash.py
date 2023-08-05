bd1d79d40e164125e20d8b496884ddbf028ce642
mjk84 <matthew_kelley@uri.edu>
2023-08-04 22:51:13 -0400

    added letter to number conversion helper method

--- rk_hash.py
+++ rk_hash.py
@@ -1,6 +1,8 @@
 from typing import List
 from letter import Letter
 
+
+
 def get_hash_letter_list(input: List[Letter]) -> int:
     hash: int = 0
     for letter in input:
@@ -10,8 +12,7 @@ def get_hash_letter_list(input: List[Letter]) -> int:
 def letter_calculation(hash: int, letter: str):
     ALPHABET_SIZE: int = 26
     PRIME: int = 97
-    ASCII_DIFFERENCE: int = 65
-    return (ALPHABET_SIZE * hash + (ord(letter[0]) - ASCII_DIFFERENCE)) % PRIME
+    return (ALPHABET_SIZE * hash + letter_to_number(letter[0])) % PRIME
 
 def get_hash_string(input: str) -> int:
     hash: int = 0
@@ -20,5 +21,16 @@ def get_hash_string(input: str) -> int:
         hash = letter_calculation(hash, letter)
     return hash
 
+def get_hash_scale(length: int) -> int:
+    ALPHABET_SIZE: int = 26
+    return ALPHABET_SIZE ** (length - 1)
+
+
 def rehash(hash: int, new_char: str, old_char: str) -> int:
+    shifted_hash: int = 0
+    back_hash: int = 0
     return hash
+
+def letter_to_number(char: str) -> int:
+    ASCII_DIFFERENCE: int = 65
+    return ord(char[0]) - ASCII_DIFFERENCE
